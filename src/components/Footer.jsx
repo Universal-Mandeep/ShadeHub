37e90ba5e957f49eadf6613e2ee3e74f23658196
MANDEEP SINGH <73429530+Universal-Mandeep@users.noreply.github.com>
2022-08-11 18:51:33 +0530

    Added files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './css/Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+
+      <div className='contact-us-label'>Contact Us</div>
+      <div className='creater-box'>
+        <div className='creater creater1'>
+          <div className='creater-name'>Mandeep Singh</div>
+          <div className='creater-contact-info'>
+
+            <div className='creater-email'>mandeepsingh1062002@gmail.com</div>
+            <div className='creater-github'>X Github</div>
+            <div className='creater-linkedin'>X LinkedIn</div>
+          </div>
+        </div>
+        
+        <div className='creater creater2'>
+        <div className='creater-name'>Mandeep Singh</div>
+          <div className='creater-contact-info'>
+
+            <div className='creater-email'>mandeepsingh1062002@gmail.com</div>
+            <div className='creater-github'>X Github</div>
+            <div className='creater-linkedin'>X LinkedIn</div>
+          </div>  
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
